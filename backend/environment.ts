1f8c66d0e2483a475eaa196f88712e97eb0c6f8b
Wesley Petitemberte <wesleypetitemberte@gmail.com>
2025-03-27 16:02:28 -0300

    Fix: Indent

--- backend/environment.ts
+++ backend/environment.ts
@@ -1,7 +1,7 @@
 export const environment: Record<string, string> = {
-    DB_HOST: 'localhost',
-    DB_USER: 'root',
-    DB_PASS: 'root',
-    DB_NAME: 'crud_db',
-    PORT: '3000'
+  DB_HOST: 'localhost',
+  DB_USER: 'root',
+  DB_PASS: 'root',
+  DB_NAME: 'crud_db',
+  PORT: '3000'
 }
