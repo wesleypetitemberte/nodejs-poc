9165ddf7e0b639412206c9819c4e06bded30e1f8
Wesley Petitemberte <wesleypetitemberte@gmail.com>
2025-04-21 19:58:41 -0300

    Chore: Removed enviroments from git

--- backend/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment: Record<string, string> = {
-  DB_HOST: 'infaze-nodejs-poc.c8r0geow49th.us-east-1.rds.amazonaws.com',
-  DB_USER: 'infaze',
-  DB_PASS: 'teste12345',
-  DB_NAME: 'crud_db',
-  PORT: '3000',
-  API_KEY: '',
-  JWT_SECRET: '',
-}
--- frontend/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment: Record<string, string> = {
-  ENDPOINT_API: 'http://localhost:3000/api/v1',
-  API_KEY: '',
-  JWT_SECRET: '',
-}
